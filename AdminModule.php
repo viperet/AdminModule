6b7c0b1114142392fed025a8a0145a539b69587e
Vitaly Peretiatko <viperet@gmail.com>
2014-11-25 21:49:53 +0200

    Renamed Database class to avoid name conflicts

--- AdminModule.php
+++ AdminModule.php
@@ -13,8 +13,10 @@ function AdminModuleAutoloader($class) {
 
 spl_autoload_register('AdminModuleAutoloader');
 require_once("Form.class.php");			
-//require_once("Pager.class.php");			
-require_once(PATH_ROOT ."classes/pageSplit.class.php");
+require_once("AdminDatabase.class.php");			
+require_once("Navigation.class.php");			
+require_once("Pagination.class.php");			
+//require_once(PATH_ROOT ."classes/pageSplit.class.php");
 
 class AdminModule {
 	var $options = array(
@@ -22,7 +24,9 @@ class AdminModule {
 			'form' => '',
 			'perpage' => 25,
 			'title' => '',
+			'sort' => '',
 		);
+	public $db, $navigation;
 	public $form;
 	public $baseUrl;
 	public $baseUrlNoPaging;
@@ -30,13 +34,21 @@ class AdminModule {
 	public $itemsCount;
 	public $filter = "";
 	function __construct($options) {
-		global $db;
+//		global $this->db;
+//! BANKER
 		
 		$this->options = array_merge($this->options, $options);	
+
+		if(empty($this->options['db'])) throw new Exception("Empty required parameter 'db'");
+		$this->db = new AdminDatabase($this->options['db']);
+		$this->navigation = new Navigation();
+
 		if(isset($this->options['baseUrl']))
 			$this->baseUrlNoFilter = $this->baseUrlNoPaging = $this->baseUrl = $this->options['baseUrl'];
-		else
-			$this->baseUrlNoFilter = $this->baseUrlNoPaging = $this->baseUrl = "/admin/datarouter.php?action=edit&id=".(int)$_GET['id'];
+//		else
+//			$this->baseUrlNoFilter = $this->baseUrlNoPaging = $this->baseUrl = "/admin/datarouter.php?action=edit&id=".(int)$_GET['id'];
+//! BANKER
+
 		
 		if(isset($_GET['block']))
 			$this->baseUrl .= "&block=".$_GET['block'];
@@ -47,7 +59,7 @@ class AdminModule {
 		
 		foreach($this->options['form'] as $name=>&$array) {
 			$className = $array['type']."Type";
-			$array = new $className($name, $array);
+			$array = new $className($this->db, $name, $array);
 			$array->options = $this->options;
 		}
 		unset($array);
@@ -58,19 +70,21 @@ class AdminModule {
 /* ================================= */
 	
 	function listItems() {
-		global $db;
+		
 
-		$page = (empty($_GET['block'])?1:(int)$_GET['block']);
 		$per_page  = $this->options['perpage'];
-		$limit = ($page-1);
+		$limit = (empty($_GET['s'])?0:(int)$_GET['s']);
 		
 		$items = $this->getItems($limit, $per_page);
 //		echo "<hr>{$this->itemsCount}<hr>";
-		$pager = new pageSplit($page, $this->itemsCount, '', $per_page);		
-		$htmlPager = $pager->showNav();
+//		$pager = new pageSplit($page, $this->itemsCount, '', $per_page);		
+//		$htmlPager = $pager->showNav();
+		
+		$pager = new Pagination($this->itemsCount, $per_page);
+		$htmlPager  = $pager->display($this->baseUrlNoPaging);		
 		
 		$headers = array('id');
-		$this->options['form'] = array_merge(array('id' => new textType('id', array('type'=>'text', 'label'=>'ID'))), $this->options['form']);
+		$this->options['form'] = array_merge(array('id' => new textType($this->db, 'id', array('type'=>'text', 'label'=>'ID'))), $this->options['form']);
 
 		foreach($this->options['form'] as $key=>$value) {
 			if(!empty($value->header)) {
@@ -86,7 +100,7 @@ class AdminModule {
 /* Редактирование/добавление записи	 */
 /* ================================	 */
 	function editItem($id, $clone = false) {
-		global $db;
+		
 		$myObjectForm = $this->options['form'];
 		if($id>0) {
 			// редактирование 
@@ -122,7 +136,7 @@ class AdminModule {
 				
 				$postSql = $this->form->postSave($id, $_POST);
 				if($postSql) {
-					$res = $db->query("UPDATE ".$this->options['table']." SET ".$postSql);
+					$res = $this->db->query("UPDATE ".$this->options['table']." SET ".$postSql);
 				}
 				header("Location: ".$this->baseUrl);
 				exit;
@@ -139,15 +153,15 @@ class AdminModule {
 /* Обновление записи      */
 /* ====================== */
 	function updateItem($id, $data) {
-		global $db;
+		
 		$sql = "UPDATE ".$this->options['table']." SET ".$this->form->save($data)." WHERE id = ". (int)$id;
 //		echo $sql; exit;
-		$res =$db->query($sql);
-		if($res !== 1) {
-			echo "UPDATE error<br>";
-			echo nl2br($res->result->userinfo);
-			exit;
-		}		
+		$res =$this->db->query($sql);
+//		if($res !== 1) {
+//			echo "UPDATE error<br>";
+//			echo nl2br($res->result->userinfo);
+//			exit;
+//		}		
 		return $res;
 	}
 
@@ -155,14 +169,14 @@ class AdminModule {
 /* Вставка записи         */
 /* ====================== */
 	function insertItem($data) {
-		global $db;
+		
 		$sql = "INSERT ".$this->options['table']." SET ".$this->form->save($data);
-		$res = $db->query($sql);
-		if($res !== 1) {
-			echo "INSERT error<br>";
-			echo nl2br($res->result->userinfo);
-			exit;
-		}
+		$res = $this->db->query($sql);
+//		if($res !== 1) {
+//			echo "INSERT error<br>";
+//			echo nl2br($res->result->userinfo);
+//			exit;
+//		}
 		return mysql_insert_id();
 	}
 
@@ -195,6 +209,8 @@ class AdminModule {
 		if(preg_match('/^%(\d+)%$/', $filter, $m)) {
 			$sql[] = "`{$this->options['table']}`.`id` = '{$m[1]}'";
 		}
+		
+		if(count($sql) == 0) return '0';
 		return ' ('.implode(' OR ', $sql).') ';
 	}
 
@@ -202,9 +218,9 @@ class AdminModule {
 /* Получение значений поля для фильтрации */
 /* ====================== */
 	function getFieldValues($field) {
-		global $db;
+		
 		$sql = "SELECT `{$field->name}` value FROM `{$this->options['table']}` GROUP By `{$field->name}`";
-		$res = $db->getAll($sql);
+		$res = $this->db->getAll($sql);
 		$items = array();
 		foreach($res as $row) {
 			$field->value = $row['value'];
@@ -218,12 +234,12 @@ class AdminModule {
 /* Получение всех записей */
 /* ====================== */
 	function getItems($from=0, $count=100) {
-		global $db;
+		
 		$sql = "SELECT SQL_CALC_FOUND_ROWS * FROM {$this->options['table']} WHERE ".$this->getFilterSQL().
 			($this->options['sort']?" ORDER By {$this->options['sort']}":"").
 			" LIMIT $from,$count";
-		$items = $db->getAll($sql);
-		$this->itemsCount = $db->foundRows;
+		$items = $this->db->getAll($sql);
+		$this->itemsCount = $this->db->foundRows;
 		return $items;	
 	}
 
@@ -231,8 +247,8 @@ class AdminModule {
 /* Получение записи по ID */
 /* ====================== */
 	function getItem($id) {
-		global $db;
-		$row = $db->getRow("SELECT * FROM `".$this->options['table']."` WHERE id=?", array($id));
+		
+		$row = $this->db->getRow("SELECT * FROM `".$this->options['table']."` WHERE id=?", array($id));
 		return $row;
 	}
 
@@ -242,13 +258,13 @@ class AdminModule {
 // TODO надо сообщать полям записи об удалении, чтоб они могли подчистить за собой данные
 // например связанные записи в таблицах, картинки, теги
 	function deleteItem($ids) {
-		global $db;
+		
 		if(!is_array($ids)) $ids = array($ids);
 		
 		foreach($ids as $id) {
 			$item = $this->getItem($id);
 			$sql = "DELETE FROM `".$this->options['table']."` WHERE id = ".intval($id);
-			$res = $db->query($sql);
+			$res = $this->db->query($sql);
 			if($res !== 1) {
 				echo "DELETE error<br>";
 				echo nl2br($res->result->userinfo);
@@ -325,12 +341,12 @@ class AdminModule {
 /* Массовые действия	*/
 /* =============== */
 	function massAction($ids, $field, $value) {
-		global $db;
+		
 		if($ids == NULL) $this->returnUser();
 		if(!is_array($ids)) $ids = array($ids);
 
 		$sql = "UPDATE `".$this->options['table']."` SET `{$field}`='{$value}' WHERE id IN (".implode(',', $ids).")";
-		$res = $db->query($sql);
+		$res = $this->db->query($sql);
 		if($res !== 1) {
 			echo "massAction error<br>";
 			echo nl2br($res->result->userinfo);
@@ -349,7 +365,6 @@ class AdminModule {
 /* Обработка действий по умолчанию	*/
 /* =============== */
 	function processCommands() {
-		global $myNav;
 
 		if(isset($_REQUEST['ajaxField']) && isset($_REQUEST['ajaxMethod'])) { // обработка AJAX
 		
@@ -359,7 +374,7 @@ class AdminModule {
 		} else { // обработка обычных запросов
 
 			if($this->options['title']!='')
-				$myNav->add($this->options['title'], $this->baseUrl);
+				$this->navigation->add($this->options['title'], $this->baseUrl);
 	
 			foreach($this->options['form'] as $key=>$value) {
 				if($value->massAction && $value->type == 'checkbox') {
@@ -374,11 +389,18 @@ class AdminModule {
 			if(isset($_REQUEST['delete'])) {
 				$this->deleteItem($_REQUEST['item']);
 			} elseif(isset($_GET['edit'])) {
-				$myNav->add("Редактирование","addarticle.php");
-				pageStart('foobar',false);
+				if($_GET['edit'] == 0)
+					$this->navigation->add("Добавление","addarticle.php");
+				else
+					$this->navigation->add("Редактирование","addarticle.php");
+				echo $this->navigation->get();
+//				pageStart('foobar',false);
+//! BANKER
 				$this->editItem($_GET['edit'], isset($_GET['clone']));
 			} else {
-				pageStart('foobar',false);
+				echo $this->navigation->get();
+//				pageStart('foobar',false);
+//! BANKER
 				$this->listItems();
 			}
 		}
