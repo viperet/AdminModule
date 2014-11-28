185668244813f9ebe1f0b186b8b6aad94094dd32
Vitaly Peretiatko <viperet@gmail.com>
2014-11-29 01:36:15 +0200

    bootstrap interface for image uploading

--- types/imageType.php
+++ types/imageType.php
@@ -19,6 +19,9 @@ class imageType extends coreType {
 	public function toString() {
 		return 'картинко';		
 	}
+
+	public function fromForm($value) {
+	}	
 	
 	public function fromRow($row) {
 		parent::fromRow($row);
@@ -26,15 +29,25 @@ class imageType extends coreType {
 			$this->value = $this->path.$this->value;
 	}	
 	public function toHtml() {
-		return ($this->width>0&&$this->height>0?"<div class='upload_field'>Размер не менее {$this->width}x{$this->height}<br>":"Произвольный размер<br>"). // width:{$this->width}px;height:{$this->height}px;
-"<div class='img_mask' style='position:relative;'>
+		return ($this->width>0&&$this->height>0?"<div class='row upload_field'>
+		<div class='col-sm-12'><p class='form-control-static'>Размер не менее {$this->width}x{$this->height}<br>":"Произвольный размер<br>")."</p></div>".
+"
+<div class='col-sm-3 col-xs-3 img_mask' style='position:relative;'>
 	<img id='{$this->name}' class='form_thumbnail {$this->class}' style='".($this->value==''?"display:none;'":"' src='{$this->value}'")." id='{$this->name}_uploadPreview' data-width='{$this->width}' data-height='{$this->height}'>
-</div>Клинки по картинке для обрезки<br>
-<button class='upload_remove' type='button'>Удалить картинку</button><br>
-<span class='link' onClick='return searchPopup(\"{$this->name}_url\", \"{$this->size}\");'>найти картинку</span> или загрузить картинку по ссылке<br>
-<input type='hidden' name='{$this->name}' value='{$this->value}'>
-<input type='text' class='form_input upload_url' name='{$this->name}_url' id='{$this->name}_url' placeholder='http://' value='{$this->value_url}'><br>
-<input type='file' class='form_input upload_image' name='{$this->name}_file' id='{$this->name}_file' placeholder='Загрузка файла'>
+</div>
+<div class='col-sm-9 col-xs-9'>
+	<div>Кликни по картинке для обрезки</div>
+	<button class='btn btn-default upload_remove' type='button'>Удалить картинку</button>
+	<span class='btn btn-default btn-file'>
+	    Загрузить с компьютера <input type='file' class='form_input upload_image' name='{$this->name}_file' id='{$this->name}_file' placeholder='Загрузка файла'>
+	</span>
+	<div>
+		<span class='link' onClick='return searchPopup(\"{$this->name}_url\", \"{$this->size}\");'>найти картинку</span> или загрузить картинку по ссылке<br>
+		<input type='hidden' name='{$this->name}' value='{$this->value}'>
+		<input type='text' class='form-control upload_url' name='{$this->name}_url' id='{$this->name}_url' placeholder='http://' value='{$this->value_url}'>
+	</div>
+</div>
+<input type='hidden' id='{$this->name}_size' value='{$this->size}' />
 <input type='hidden' id='{$this->name}_x' name='{$this->name}_x' />
 <input type='hidden' id='{$this->name}_y' name='{$this->name}_y' />
 <input type='hidden' id='{$this->name}_w' name='{$this->name}_w' />
@@ -52,13 +65,13 @@ class imageType extends coreType {
 		$this->w = $params[$this->name.'_w'];
 		$this->h = $params[$this->name.'_h'];
 
-		if($subfolders)
+		if($this->subfolders)
 			$relative_path = chunk_split(substr(str_pad($id, 4, '0', STR_PAD_LEFT), 0, 4), 2,'/');
 		else
 			$relative_path = '';
 		
 	    $path = $this->path.$relative_path;		
-	    @mkdir(PATH_ROOT.$path, 0777, true);
+	    @mkdir($this->options['root_path'].$path, 0777, true);
 	    $fname = str_replace(array(
 	    			'{id}',
 	    			'{width}',
@@ -75,8 +88,8 @@ class imageType extends coreType {
 		} elseif(!empty($_FILES[$this->name.'_file']['tmp_name'])) {
 			$fileName = $_FILES[$this->name.'_file']['tmp_name'];
 		} elseif(!empty($params[$this->name.'_remove'])) { // удаление картинки
-			@unlink(PATH_ROOT.$path.$fname.".jpg");
-			@unlink(PATH_ROOT.$path.$fname.".png");
+			@unlink($this->options['root_path'].$path.$fname.".jpg");
+			@unlink($this->options['root_path'].$path.$fname.".png");
 			return "`{$this->name}` = ''";
 		} else 
 			return "";
@@ -99,7 +112,7 @@ class imageType extends coreType {
 	    	die("неизвестный формат файла в поле {$this->label}, сигнатура $signature");
 	    }
 	    
-		$name= PATH_ROOT.$path.$fname.".".$ext; // имя файла
+		$name= $this->options['root_path'].$path.$fname.".".$ext; // имя файла
 		if($this->relative)
 			$url = $fname.".".$ext."?v=".time(); // ссылка на файл
 		else
@@ -132,20 +145,112 @@ class imageType extends coreType {
 	public static function pageHeader() {
 ?>
 <style>
-.img_mask {
-	float: left;
-	margin-right: 20px;
-/*	position: relative;
-	background: #FFF url(/site_img/nophoto.jpg) center center no-repeat;
-	overflow: hidden;
+	.img_mask {
+		cursor: pointer;
+	/*	position: relative;
+		background: #FFF url(/site_img/nophoto.jpg) center center no-repeat;
+		overflow: hidden;
+	*/
+	}
+	.img_mask img {
+	/*	position: absolute; */
+		max-width: 100%;
+		max-height: 200px;
+	}
+	.searchResult {
+		width: 25%;
+		height: 30%;
+		text-align: center;
+		vertical-align: middle;
+		display: inline-block;
+		padding: 3px;
+		vertical-align: bottom;
+	}
+	/* Small devices (tablets, 768px and up) */
+	@media (max-width: 992px) {
+		.searchResult {
+			width: 33%;
+		}
+	}
+	@media (max-width: 768px) {
+		.searchResult {
+			width: 50%;
+		}
+	}
+	.searchResult img {
+		max-width: 100%;
+		max-height: 100%;
+	}
+	.searchResult div {
+		font-size: 12px;
+	}
+	.searchPages {
+	    padding: 10px 0 0;
+	    text-align: center;
+	}
+	.searchPage {
+	    background: none repeat scroll 0 0 #e5e5e5;
+	    color: gray;
+	    cursor: pointer;
+	    display: inline-block;
+	    height: 20px;
+	    line-height: 15px;
+	    margin: 2px;
+	    padding: 3px;
+	}
+	.searchPage.active {
+		font-weight: bold;
+		color: black;
+	}
+/*
+	#imageCrop #previewCrop {
+		max-width: 100%;
+	}
 */
-}
-.img_mask img {
-/*	position: absolute; */
-	max-width: 200px;
-	max-height: 200px;
-}
 </style>
+<script src="http://www.google.com/jsapi" type="text/javascript"></script>
+<div class="modal fade" id="imageSearch" tabindex="-1" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Закрыть</span></button>
+        <h4 class="modal-title" id="exampleModalLabel">Поиск изображения</h4>
+        <form id="searchForm" role="form">
+	        <div class="input-group">
+				<input type="text" id="q" class="form-control"> 
+				<span class="input-group-btn">
+					<button class="btn btn-primary" type="submit" id="search">Искать</button>
+				</span>
+	        </div>
+		</form>
+      </div>
+      <div class="modal-body">
+	  	<div id="searchResults" class="clearfix"></div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="modal fade" id="imageCrop" tabindex="-1" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Закрыть</span></button>
+        <h4 class="modal-title" id="exampleModalLabel">Обрезка изображения</h4>
+      </div>
+      <div class="modal-body">
+
+			<img src="" id="previewCrop">
+
+      </div>
+      <div class="modal-footer">
+		<button type="btn btn-primary" id="apply" data-dismiss="modal">Применить</button>
+      </div>      
+    </div>
+  </div>
+</div>
+
 <script>
 function cropImage(id) {
 	var img = $('#'+id);
@@ -174,12 +279,14 @@ function cropImage(id) {
 		selectSize.y = Math.floor((imgSize.h-selectSize.h)/2);
 	}
 
+	
 
 //		img.width(selectSize.w).height(selectSize.h).css('top', selectSize.y).css('left', selectSize.x);
 	$('#'+id+'_x').val(selectSize.x);
 	$('#'+id+'_y').val(selectSize.y);
 	$('#'+id+'_w').val(selectSize.w);
 	$('#'+id+'_h').val(selectSize.h);
+	return selectSize;
 }
 
 
@@ -239,23 +346,18 @@ $(function() {
 
 
 });
-</script>
-<?	
-	
-		$search_popup = <<< EOT
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
-<script src="http://www.google.com/jsapi" type="text/javascript"></script>
-<script type="text/javascript">
-	function search(start) {
-		query = encodeURIComponent($('#q').val());
+
+
+	function search(id, start) {
+		var query = encodeURIComponent($('#imageSearch #q').val());
+		var size = encodeURIComponent($('#'+id+'_size').val());
 		$.getJSON('https://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=8&start='+start+'&imgsz=image_size&q='+query+'&callback=?', function (data) {
-			sr = $('#searchResults');
+			var sr = $('#searchResults');
 			sr.empty();
+			var results = $('<div class="searchResults"></div>').appendTo(sr);
+			var pages = $('<div class="searchPages"></div>').appendTo(sr);
 			$.each(data.responseData.results, function () {
-				sr.append("<div class='searchResult'>"+
+				results.append("<div class='searchResult'>"+
 						"<a href='"+this.url+"'>"+
 						"<img src='"+this.tbUrl+"'>"+
 						"<div>"+this.width+"x"+this.height+"</div>"+
@@ -264,158 +366,79 @@ $(function() {
 				);
 			})
 			$.each(data.responseData.cursor.pages, function () {
-				sr.append("<div class='searchPage "+(start == this.start?'active':'')+"' onClick='search("+this.start+")'>"+
+				pages.append("<div class='searchPage "+(start == this.start?'active':'')+"' onClick='search(\""+id+"\", "+this.start+")'>"+
 						this.label+
 					"</div"
 				);
 			})
 		})
 	}
-	$(function () {
-		$('#searchForm').submit(function (e) {
-			e.preventDefault();
-			search(0);
-		});
-        $('#searchResults').on("click", ".searchResult a", function () {
-        	var href = $(this).attr('href');
-        	var id = 'image_url';
-        	var i = window.opener.document.getElementById(id);
-        	i.value=href;
-        	window.opener.$('#'+id).trigger('change');
-        	window.close();
-        	return false;
-        })
-		$('#q').val('image_title');
-		$('#searchForm').submit();
-	});
-</script>
-</head>
-<body>
-<style>
-	.searchResult {
-		width: 150px;
-		height: 150px;
-		text-align: center;
-		vertical-align: middle;
-		float: left;
-		padding: 3px;
-	}
-	.searchPage {
-		cursor: pointer;
-		color: gray;
-		height: 20px;
-	}
-	.active {
-		font-weight: bold;
-		color: black;
-	}
-</style>
-<form id="searchForm">
-	<input type="text" id="q" style="width:300px;"> <button class="btn btn-primary" type="submit" id="search">Искать</button>
-</form>
-<div id="searchResults"></div>
-</body>
-</html>
-EOT;
-	
-	
-echo "
-<script>
-	function searchPopup(result_id, size) {
-		title = $('#name').val() || $('#title').val() || $('#family').val() || '';
-		popupWin = window.open('', 'search', 'location,width=700,height=400,top=0');
-		dialog = '".strtr($search_popup, array('\\'=>'\\\\',"'"=>"\\'",'"'=>'\\"',"\r"=>'\\r',"\n"=>'\\n','</'=>'<\/'))."';
-		dialog = dialog.replace(/image_url/i, result_id);
-		dialog = dialog.replace(/image_title/i, title);
-		dialog = dialog.replace(/image_size/i, size);
-		popupWin.document.write(dialog);
-		popupWin.document.close();
-		
-		popupWin.focus();
-		return false;
-	}
-</script>
-";
 
-		$crop_popup = <<< EOT
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
-<link href="/js/jq/jcrop/jquery.Jcrop.css" rel="stylesheet" type="text/css">
-<script type="text/javascript" src="/js/jq/jcrop/jquery.Jcrop.js"></script>
-<script type="text/javascript">
+$('#imageSearch #searchForm').submit(function (e) {
+	var id = $('#imageSearch').data('id');
+	e.preventDefault();
+	search(id, 0);
+});
+$('#imageSearch #searchResults').on("click", ".searchResult a", function () {
+	var id = $('#imageSearch').data('id');
+	var href = $(this).attr('href');
+	$('#'+id).val(href).trigger('change');
+	$('#imageSearch').modal('hide')
+	return false;
+})
+
+
+function searchPopup(result_id, size) {
+	title = $('#name').val() || $('#title').val() || $('#family').val() || '';
+	$('#imageSearch #q').val(title);
+	$('#imageSearch #searchForm').submit();		
+	$('#imageSearch').data('id', result_id).modal();
+	return false;
+}
+
+function cropPopup(id, image, image_width, image_height, x,y,w,h) {
 	var Jcrop;
-	
-	$(function () {
-		var img = $('#preview');
-		img.attr('src', window.opener.$('#image_id').attr('src'));
+	if($('#'+id+'_w').val() == '') cropImage(id);
 
-		var targetSize = {w: image_width, h: image_height, aspect: image_width/image_height};
-		
+	var image_x = $('#'+id+'_x').val(),
+	    image_y = $('#'+id+'_y').val(),
+	    image_w = $('#'+id+'_w').val(),
+	    image_h = $('#'+id+'_h').val();
+	
+	var img = $('#imageCrop #previewCrop');
+
+	var targetSize = {w: image_width, h: image_height, aspect: image_width/image_height};
+
+	$('#imageCrop').modal().on('shown.bs.modal', function () {
+		var max_width = $('#imageCrop .modal-body').width();
+		img.attr('src', $('#'+id).attr('src'));
 		$(img).Jcrop({
 			minSize: [targetSize.w, targetSize.h],
 			aspectRatio: targetSize.aspect,
-			boxWidth: 800,
+			boxWidth: max_width,
 			boxHeight: 600,
 			setSelect: [image_x, image_y, image_x+image_w, image_y+image_h]
 		},function(){
 			Jcrop = this;
-		});
-		
-		$('#apply').click(function () {
-			var crop = Jcrop.tellSelect();
-			window.opener.$('#image_id_x').val(crop.x);
-			window.opener.$('#image_id_y').val(crop.y);
-			window.opener.$('#image_id_w').val(Math.round(crop.w));
-			window.opener.$('#image_id_h').val(Math.round(crop.h));
-        	window.close();
-        	return false;			
-		});
+		});		
 	});
-</script>
-</head>
-<body>
-<style>
-#preview {
-/*	max-width:800px;
-	max-height:600px; */
+
+	$('#apply').one('click', function () {
+		var crop = Jcrop.tellSelect();
+		$('#'+id+'_x').val(crop.x);
+		$('#'+id+'_y').val(crop.y);
+		$('#'+id+'_w').val(Math.round(crop.w));
+		$('#'+id+'_h').val(Math.round(crop.h));
+    	$('#imageCrop').modal('close');
+    	return false;			
+	});		
+
+	
+
+	return false;
 }
-</style>
-<img src="" id="preview">
-<button type="btn btn-primary" id="apply">Применить</button>
-</body>
-</html>
-EOT;
-	
-	
-echo "
-<script>
-	function cropPopup(id, image, width, height, x,y,w,h) {
-	
-		if($('#'+id+'_w').val() == '') cropImage(id);
-	
-		popupWin = window.open('', 'search', 'location,width=600,height=400,top=100,left=200');
-		dialog = '".strtr($crop_popup, array('\\'=>'\\\\',"'"=>"\\'",'"'=>'\\"',"\r"=>'\\r',"\n"=>'\\n','</'=>'<\/'))."';
-		dialog = dialog.replace(/image_id/ig, id);
-//		dialog = dialog.replace(/image_url/ig, image);
-		dialog = dialog.replace(/image_width/ig, width);
-		dialog = dialog.replace(/image_height/ig, height);
-
-		dialog = dialog.replace(/image_x/ig, $('#'+id+'_x').val());
-		dialog = dialog.replace(/image_y/ig, $('#'+id+'_y').val());
-		dialog = dialog.replace(/image_w/ig, $('#'+id+'_w').val());
-		dialog = dialog.replace(/image_h/ig, $('#'+id+'_h').val());
-		
-		popupWin.document.write(dialog);
-		popupWin.document.close();
-		
-		popupWin.focus();
-		return false;
-	}
 </script>
-";
-
+<?	
 
 	}
 
