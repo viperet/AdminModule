269836f2a5c45824fd05100a72fdc0ff9ec65581
Vitaly Peretiatko <viperet@gmail.com>
2014-12-16 22:49:09 +0200

    Readonly property

--- types/textType.php
+++ types/textType.php
@@ -2,10 +2,13 @@
 
 class textType extends coreType {
 	public $placeholder;
-
+	public $readonly = false;
 
 	public function toHtml() {
-		return "<input type='text' name='{$this->name}' id='{$this->name}' class='form-control {$this->class} ".(!$this->valid?'error':'')."' value='".$this->escape($this->value)."' ".(!empty($this->placeholder)?"placeholder='".htmlspecialchars($this->placeholder)."' ":'')." />";
+		return "<input type='text' name='{$this->name}' id='{$this->name}' class='form-control {$this->class} ".(!$this->valid?'error':'')."' value='".$this->escape($this->value)."' ".
+			(!empty($this->placeholder)?"placeholder='".htmlspecialchars($this->placeholder)."' ":'').
+			(!empty($this->readonly)?"readonly":'').
+			" />";
 	}
 	
 	public function toSql() {
