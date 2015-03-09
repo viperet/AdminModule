005e5e89d040e46845df3a871d67761725d427d2
Vitaly Peretiatko <viperet@gmail.com>
2015-03-10 00:51:40 +0200

    do not modify .po files

--- .update_translation.sh
+++ .update_translation.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-xgettext --omit-header -j -o locale/uk_UA.UTF-8/LC_MESSAGES/AdminModule.po   *.php types/*.php
+#xgettext --omit-header -j -o locale/uk_UA.UTF-8/LC_MESSAGES/AdminModule.po   *.php types/*.php
 msgfmt -o locale/uk_UA.UTF-8/LC_MESSAGES/AdminModule.mo  locale/uk_UA.UTF-8/LC_MESSAGES/AdminModule.po
 
-xgettext --omit-header -j -o locale/ru_RU.UTF-8/LC_MESSAGES/AdminModule.po *.php types/*.php
+#xgettext --omit-header -j -o locale/ru_RU.UTF-8/LC_MESSAGES/AdminModule.po *.php types/*.php
 msgfmt -o locale/ru_RU.UTF-8/LC_MESSAGES/AdminModule.mo  locale/ru_RU.UTF-8/LC_MESSAGES/AdminModule.po
 
-xgettext --omit-header -j -o locale/en_US.UTF-8/LC_MESSAGES/AdminModule.po *.php types/*.php
+#xgettext --omit-header -j -o locale/en_US.UTF-8/LC_MESSAGES/AdminModule.po *.php types/*.php
 msgfmt -o locale/en_US.UTF-8/LC_MESSAGES/AdminModule.mo  locale/en_US.UTF-8/LC_MESSAGES/AdminModule.po
