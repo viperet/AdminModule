4a590a5800df69d62606a16d24180f9c95b24343
Vitaly Peretiatko <viperet@gmail.com>
2016-03-23 14:17:49 +0200

    Changed constructor to support mysqli link id or connection paramenters

--- AdminDatabase.class.php
+++ AdminDatabase.class.php
@@ -1,36 +1,36 @@
 <?php
 
 class AdminDatabaseException extends RuntimeException {
-	const DUPLICATE_KEY = 1062;
+    const DUPLICATE_KEY = 1062;
 
-	protected $sql;
-	public function __construct ($message = "", $code = 0, $sql = "") {
-		$this->sql = $sql;
-		return parent::__construct($message, $code);
-	}
+    protected $sql;
+    public function __construct ($message = "", $code = 0, $sql = "") {
+        $this->sql = $sql;
+        return parent::__construct($message, $code);
+    }
 }
 
 class Rowset implements Iterator, Countable {
-	public $resource;
-	public $count = 0;
-	public $foundRows = 0;
-	public $affectedRows = NULL;
+    public $resource;
+    public $count = 0;
+    public $foundRows = 0;
+    public $affectedRows = NULL;
 
-	private $_position = 0;
-	private $_row = FALSE;
+    private $_position = 0;
+    private $_row = FALSE;
 
-	function __construct($resource) {
-		$this->resource = $resource;
-		$this->count = mysqli_num_rows($this->resource);
-		$this->fetchRow();
-	}
-	public function rewind() {
+    function __construct($resource) {
+        $this->resource = $resource;
+        $this->count = mysqli_num_rows($this->resource);
+        $this->fetchRow();
+    }
+    public function rewind() {
         if($this->_position != 0) {
-	        mysqli_data_seek($this->resource, 0);
-	        $this->_position = 0;
+            mysqli_data_seek($this->resource, 0);
+            $this->_position = 0;
 
-	        $this->fetchRow();
-	    }
+            $this->fetchRow();
+        }
     }
 
     public function current() {
@@ -55,79 +55,90 @@ class Rowset implements Iterator, Countable {
     }
 
     private function fetchRow() {
-		return $this->_row = mysqli_fetch_assoc($this->resource);
-	}
+        return $this->_row = mysqli_fetch_assoc($this->resource);
+    }
 
 }
 
 
 class AdminDatabase {
-	public $linkId;
-	public $insertId;
-	public $foundRows;
+    public $linkId;
+    public $insertId;
+    public $foundRows;
 
-	function __construct($linkId) {
-		$this->linkId = $linkId;
-	}
+    function __construct($linkId) {
+        $args = func_get_args();
+        if(count($args) == 1 && is_object($args[0]) && is_a($args[0], 'mysqli')) { // 1 argument - link id
+            $this->linkId = $args[0];
+        } elseif(count($args) >= 1 && is_string($args[0])) {
+            $this->linkId = call_user_func_array('mysqli_connect', $args);
+        } else {
+            var_dump($args);
+            echo "<pre>";
+            debug_print_backtrace();
+            echo "</pre>";
+            die('You should pass either "mysqli" object or connection parameters host,user,password,dbname');
+        }
+    }
 
-	function selectDb($name) {
-		mysqli_select_db($this->linkId, $name);
-	}
+    function selectDb($name) {
+        mysqli_select_db($this->linkId, $name);
+    }
 
-	static function isError($row) {
-		return false;
-	}
+    static function isError($row) {
+        return false;
+    }
 
-	function escape($values, $quote = true) {
-		if(!is_array($values))
-			return ($quote?"'":'').mysqli_real_escape_string($this->linkId, $values).($quote?"'":'');
+    function escape($values, $quote = true) {
+        if(!is_array($values))
+            return ($quote?"'":'').mysqli_real_escape_string($this->linkId, $values).($quote?"'":'');
 
-		foreach($values as &$value) {
-			$value = ($quote?"'":'').mysqli_real_escape_string($this->linkId, $value).($quote?"'":'');
-		}
-		return $values;
-	}
+        foreach($values as &$value) {
+            $value = ($quote?"'":'').mysqli_real_escape_string($this->linkId, $value).($quote?"'":'');
+        }
+        return $values;
+    }
 
 
-	function showError($row) {
-		return false;
-	}
+    function showError($row) {
+        return false;
+    }
 
-	function query($sql, $args = NULL) {
+    function query($sql, $args = NULL) {
 
-		if($this->linkId === false) return false;
+        if($this->linkId === false) return false;
 
-		if($args!==NULL && !is_array($args))
-			$args = array_slice(func_get_args(), 1);
+        if($args!==NULL && !is_array($args))
+            $args = array_slice(func_get_args(), 1);
 
-		if(is_array($args)) {
-			preg_match_all("/\?/", $sql, $matches, PREG_OFFSET_CAPTURE);
-			// if the number of items to replace is different than the number of items specified in $replacements
-			if (count($matches[0]) != count($args)) {
-				echo "<pre>";
-				debug_print_backtrace();
-				echo "</pre>";
-				echo _("Query:")." {$sql}<br>";
-				var_dump($args);
-			    die(_("Replacement count doesn't match"));
-			}
+        if(is_array($args)) {
+            preg_match_all("/\?/", $sql, $matches, PREG_OFFSET_CAPTURE);
+            // if the number of items to replace is different than the number of items specified in $replacements
+            if (count($matches[0]) != count($args)) {
+                echo "<pre>";
+                debug_print_backtrace();
+                echo "</pre>";
+                echo _("Query:")." {$sql}<br>";
+                var_dump($args);
+                die(_("Replacement count doesn't match"));
+            }
 
-		}
+        }
 
         $backtraceInfo = debug_backtrace();
         // get some variables from the back trace array
         for($i=0;$i<10;$i++) {
-	        $callerFile = $backtraceInfo[$i]["file"];
-	        $callerLine = $backtraceInfo[$i]["line"];
-	        $callerMethod = @$backtraceInfo[$i+1]["function"];
-	        $baseName =basename($callerFile);
-	        if($baseName!='AdminDatabase.class.php' && $baseName!='NewsEvents.class.php') break;
-	    }
+            $callerFile = $backtraceInfo[$i]["file"];
+            $callerLine = $backtraceInfo[$i]["line"];
+            $callerMethod = @$backtraceInfo[$i+1]["function"];
+            $baseName =basename($callerFile);
+            if($baseName!='AdminDatabase.class.php' && $baseName!='NewsEvents.class.php') break;
+        }
         // unset the backtrace array
         unset($backtraceInfo);
 
         // executes the query
-		$callerFileRel = str_replace(ROOT_PATH, '', $callerFile);
+        $callerFileRel = str_replace(ROOT_PATH, '', $callerFile);
 
         if(is_array($args)) {
             if($stmt = mysqli_prepare($this->linkId, "/* {$callerFileRel}:{$callerLine} {$callerMethod}() */ ".$sql)) {
@@ -142,133 +153,133 @@ class AdminDatabase {
                 mysqli_stmt_execute($stmt);
                 $res = mysqli_stmt_get_result($stmt);
             } else {
-    			echo "<pre>";
-    			debug_print_backtrace();
-    			echo "</pre>";
-    			echo _("Query:")." {$sql}<br>";
-    		    die(_('Error preparing query:').' ' . mysqli_stmt_error($this->linkId));
-    		}
+                echo "<pre>";
+                debug_print_backtrace();
+                echo "</pre>";
+                echo _("Query:")." {$sql}<br>";
+                die(_('Error preparing query:').' ' . mysqli_stmt_error($this->linkId));
+            }
         } else {
             if(!$res = mysqli_query($this->linkId, "/* {$callerFileRel}:{$callerLine} {$callerMethod}() */ ".$sql)) {
-    			echo "<pre>";
-    			debug_print_backtrace();
-    			echo "</pre>";
-    			echo _("Query:")." {$sql}<br>";
-    		    die(_('Error executing query:').' ' . mysqli_error($this->linkId));
-    		}
+                echo "<pre>";
+                debug_print_backtrace();
+                echo "</pre>";
+                echo _("Query:")." {$sql}<br>";
+                die(_('Error executing query:').' ' . mysqli_error($this->linkId));
+            }
         }
 
-		if(is_object($res)) { // SELECT, SHOW, DESCRIBE, EXPLAIN
-			if(strpos($sql, 'SQL_CALC_FOUND_ROWS') > 0) {
-				$this->foundRows = $this->getOne("SELECT FOUND_ROWS()");
-			} else {
-				$this->foundRows = 0;
-			}
-			$rowset = new Rowset($res);
-			$rowset->foundRows = $this->foundRows;
-			return $rowset;
-		} elseif(is_bool($res)) {
-			return $res;
-		} else { // INSERT, UPDATE, DELETE, DROP
-			$this->insertId = mysqli_insert_id($this->linkId);
-			$this->affectedRows = mysqli_affected_rows($this->linkId);
-			return $this->insertId;
-		}
+        if(is_object($res)) { // SELECT, SHOW, DESCRIBE, EXPLAIN
+            if(strpos($sql, 'SQL_CALC_FOUND_ROWS') > 0) {
+                $this->foundRows = $this->getOne("SELECT FOUND_ROWS()");
+            } else {
+                $this->foundRows = 0;
+            }
+            $rowset = new Rowset($res);
+            $rowset->foundRows = $this->foundRows;
+            return $rowset;
+        } elseif(is_bool($res)) {
+            return $res;
+        } else { // INSERT, UPDATE, DELETE, DROP
+            $this->insertId = mysqli_insert_id($this->linkId);
+            $this->affectedRows = mysqli_affected_rows($this->linkId);
+            return $this->insertId;
+        }
 
 
-	}
+    }
 
-	function getOne($sql, $args = NULL) {
-		if($args!==NULL && !is_array($args))
-			$args = array_slice(func_get_args(), 1);
-		$res = $this->query($sql, $args);
-		if($res->count == 0) return NULL;
-		$row = $res->current();
-		return current($row);
-	}
+    function getOne($sql, $args = NULL) {
+        if($args!==NULL && !is_array($args))
+            $args = array_slice(func_get_args(), 1);
+        $res = $this->query($sql, $args);
+        if($res->count == 0) return NULL;
+        $row = $res->current();
+        return current($row);
+    }
 
-	function getRow($sql, $args = NULL) {
-		if($args!==NULL && !is_array($args))
-			$args = array_slice(func_get_args(), 1);
-		$res = $this->query($sql, $args);
-		$row = $res->current();
-		return $row;
-	}
+    function getRow($sql, $args = NULL) {
+        if($args!==NULL && !is_array($args))
+            $args = array_slice(func_get_args(), 1);
+        $res = $this->query($sql, $args);
+        $row = $res->current();
+        return $row;
+    }
 
-	function getAll($sql, $args = NULL) {
-		if($args!==NULL &&!is_array($args))
-			$args = array_slice(func_get_args(), 1);
-		$res = $this->query($sql, $args);
-		$data = array();
-		foreach($res as $row) {
-			if(isset($row['id']))
-				$data[$row['id']] = $row;
-			else
-				$data[] = $row;
-		}
-		return $data;
-	}
+    function getAll($sql, $args = NULL) {
+        if($args!==NULL &&!is_array($args))
+            $args = array_slice(func_get_args(), 1);
+        $res = $this->query($sql, $args);
+        $data = array();
+        foreach($res as $row) {
+            if(isset($row['id']))
+                $data[$row['id']] = $row;
+            else
+                $data[] = $row;
+        }
+        return $data;
+    }
 
 
-	// Executes SQL and returns list of values of first SQL result field
-	function getList($sql, $args = NULL) {
-		if($args!==NULL &&!is_array($args))
-			$args = array_slice(func_get_args(), 1);
-		$res = $this->query($sql, $args);
-		$data = array();
-		foreach($res as $row) {
-				$data[] = reset($row);
-		}
-		return $data;
-	}
+    // Executes SQL and returns list of values of first SQL result field
+    function getList($sql, $args = NULL) {
+        if($args!==NULL &&!is_array($args))
+            $args = array_slice(func_get_args(), 1);
+        $res = $this->query($sql, $args);
+        $data = array();
+        foreach($res as $row) {
+                $data[] = reset($row);
+        }
+        return $data;
+    }
 
-	function replace($table, $fields) {
-		$sql = "REPLACE $table SET ";
-		$i = 0;
-		foreach($fields as $key=>$value) {
-			$sql .= "`{$key}` = ".AdminDatabase::escape($value);
-			if(++$i !== count($fields)) $sql .= ', ';
-		}
-		return $this->query($sql); // return inserted id
-	}
-	function insert($table, $fields) {
-		$sql = "INSERT $table SET ";
-		$i = 0;
-		foreach($fields as $key=>$value) {
-			$sql .= "`{$key}` = ".AdminDatabase::escape($value);
-			if(++$i !== count($fields)) $sql .= ', ';
-		}
-		return $this->query($sql); // return inserted id
-	}
-	function insertIgnore($table, $fields) {
-		return $this->insert("IGNORE ".$table, $fields);
-	}
+    function replace($table, $fields) {
+        $sql = "REPLACE $table SET ";
+        $i = 0;
+        foreach($fields as $key=>$value) {
+            $sql .= "`{$key}` = ".AdminDatabase::escape($value);
+            if(++$i !== count($fields)) $sql .= ', ';
+        }
+        return $this->query($sql); // return inserted id
+    }
+    function insert($table, $fields) {
+        $sql = "INSERT $table SET ";
+        $i = 0;
+        foreach($fields as $key=>$value) {
+            $sql .= "`{$key}` = ".AdminDatabase::escape($value);
+            if(++$i !== count($fields)) $sql .= ', ';
+        }
+        return $this->query($sql); // return inserted id
+    }
+    function insertIgnore($table, $fields) {
+        return $this->insert("IGNORE ".$table, $fields);
+    }
 
-	function insertUpdate($table, $fields) {
-		$sql = "INSERT $table SET ";
-		$i = 0;
-		foreach($fields as $key=>$value) {
-			$sql .= "`{$key}` = ".AdminDatabase::escape($value);
-			if(++$i !== count($fields)) $sql .= ', ';
-		}
-		$i = 0;
-		$sql .= ' ON DUPLICATE KEY UPDATE ';
-		foreach($fields as $key=>$value) {
-			$sql .= "`{$key}` = VALUES(`{$key}`)";
-			if(++$i !== count($fields)) $sql .= ', ';
-		}
-		return $this->query($sql); // return inserted id
-	}
+    function insertUpdate($table, $fields) {
+        $sql = "INSERT $table SET ";
+        $i = 0;
+        foreach($fields as $key=>$value) {
+            $sql .= "`{$key}` = ".AdminDatabase::escape($value);
+            if(++$i !== count($fields)) $sql .= ', ';
+        }
+        $i = 0;
+        $sql .= ' ON DUPLICATE KEY UPDATE ';
+        foreach($fields as $key=>$value) {
+            $sql .= "`{$key}` = VALUES(`{$key}`)";
+            if(++$i !== count($fields)) $sql .= ', ';
+        }
+        return $this->query($sql); // return inserted id
+    }
 
-	function update($table, $id, $fields) {
-		$sql = "UPDATE $table SET ";
-		$i = 0;
-		foreach($fields as $key=>$value) {
-			$sql .= "`{$key}` = ".AdminDatabase::escape($value);
-			if(++$i !== count($fields)) $sql .= ', ';
-		}
-		$sql .= " WHERE id = ".AdminDatabase::escape($id);
-		return $this->query($sql); // return inserted id
-	}
+    function update($table, $id, $fields) {
+        $sql = "UPDATE $table SET ";
+        $i = 0;
+        foreach($fields as $key=>$value) {
+            $sql .= "`{$key}` = ".AdminDatabase::escape($value);
+            if(++$i !== count($fields)) $sql .= ', ';
+        }
+        $sql .= " WHERE id = ".AdminDatabase::escape($id);
+        return $this->query($sql); // return inserted id
+    }
 
 }
