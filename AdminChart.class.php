9216ea46c5ea772f6adbfcbb6446e41807cf0bc7
Vitaly Peretiatko <viperet@gmail.com>
2016-03-31 10:19:17 +0300

    Improvements in charts

--- AdminChart.class.php
+++ AdminChart.class.php
@@ -1,29 +1,30 @@
 <?php
-	
+
 class AdminChart extends AdminModule {
 	public $series = [];
 	public $xAxis = [];
 	public $filter_fields = [];
 
-	
+
 	function __construct($options) {
 		if(empty($options['type'])) $options['type'] = 'line';
 		if(empty($options['date_default'])) $options['date_default'] = 7;
+		if(empty($options['height'])) $options['height'] = 400;
 		parent::__construct($options);
-		if(empty($_GET['df']) && empty($_GET['dt'])) { 
+		if(empty($_GET['df']) && empty($_GET['dt'])) {
 			$this->dateFrom = date('Y-m-d', time()-($options['date_default'] * 24 * 60 * 60));
 			$this->dateTo = date('Y-m-d');
 			$this->baseUrl .= "&df=".urlencode($_GET['df'])."&dt=".urlencode($_GET['dt']);
 			$this->baseUrlNoPaging .= "&df=".urlencode($_GET['df'])."&dt=".urlencode($_GET['dt']);
-		}		
+		}
 	}
-	
+
 	function listItems() {
 		$this->sortFields();
 
 		$per_page  = $this->options['perpage'];
 		$limit = (empty($_GET['s'])?0:(int)$_GET['s']);
-		
+
 		$items = $this->getItems($limit, $per_page);
 
 		foreach($this->options['form'] as $key=>$value) {
@@ -32,44 +33,44 @@ class AdminChart extends AdminModule {
 				foreach($items as $item) {
 					$series['data'][] = $item[$value->name];
 				}
-				
+
 				$this->series[] = $series;
 			}
 			if(!empty($value->filterByClick)) {
 				$this->filter_fields[] = $value;
 			}
 		}
-		
+
 		$xaxis_value = $this->options['form'][$this->options['xaxis']];
 		foreach($items as $item) {
 			$xaxis_value->fromRow($item);
 			$this->xAxis[] = $xaxis_value->toString();
-		}		
-		
+		}
+
 		include('views/chart_template.php');
 
-	}	
-	
+	}
+
 	function draw() {
 		if($this->options['title']!='')
 			$this->navigation->add($this->options['title'], $this->baseUrl);
 		echo $this->navigation->get();
 		$this->listItems();
-		
+
 	}
-	
+
 	function json($data) {
 		return json_encode($data, JSON_NUMERIC_CHECK | JSON_UNESCAPED_UNICODE);
 	}
-	
+
 	function updateItem($id, $data) {
 		return false;
-	}	
+	}
 	function insertItem($data) {
 		return false;
 	}
 	function processCommands() {
 		return false;
 	}
-	
+
 }
