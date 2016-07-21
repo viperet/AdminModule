df5814f8081ade3531ac71819e6998b927fc2e94
Перетятько Виталий <viperet@gmail.com>
2016-07-21 12:08:01 +0300

    Added autocomplete property for text fields

--- types/textType.php
+++ types/textType.php
@@ -4,18 +4,20 @@ class textType extends coreType {
 	public $placeholder;
 	public $readonly = false;
 	public $values = array();
+	public $autocomplete = true;
 
 	public function toHtml() {
 		return "<input type='text' name='{$this->name}' id='{$this->name}' class='form-control {$this->class} ".(!$this->valid?'error':'')."' value='".$this->escape($this->value)."' ".
 			(!empty($this->placeholder)?"placeholder='".htmlspecialchars($this->placeholder)."' ":'').
 			(!empty($this->readonly)?"readonly":'').
+			(!$autocomplete?"autocomplete='nope'":'').
 			" />";
 	}
-	
+
 	public function toSql() {
 		if($this->readonly) return "";
 		return "`{$this->name}`=".$this->db->escape($this->value);
-		
+
 	}
 	public function getValues() {
 		if(count($this->values)>0) {
@@ -23,5 +25,5 @@ class textType extends coreType {
 		} else {
 			return parent::getValues();
 		}
-	}	
+	}
 }
