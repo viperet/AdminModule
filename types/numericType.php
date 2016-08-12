006714b477490d5a34a0a38838414099bc93ffb9
Vitaly Peretiatko <viperet@gmail.com>
2016-08-12 16:08:33 +0300

    Typo fixed

--- types/numericType.php
+++ types/numericType.php
@@ -23,7 +23,7 @@ class numericType extends textType {
 		if($this->value === "" || $this->value === NULL)
 			return "`{$this->name}`=NULL";
 		else
-			return "`{$this->name}`=".$this->db->escaoe($this->value);
+			return "`{$this->name}`=".$this->db->escape($this->value);
 		
 	}	
 }
