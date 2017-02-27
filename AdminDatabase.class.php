531399b3bfacfa35443b5d17c6b56c9b42411984
Vitaly Peretiatko <viperet@gmail.com>
2017-02-27 14:50:44 +0000

    Fixed error in $db->query

--- AdminDatabase.class.php
+++ AdminDatabase.class.php
@@ -176,7 +176,7 @@ class AdminDatabase {
                     else
                         $types .= 's';
                 }
-                call_user_func_array('mysqli_stmt_bind_param', array_merge(array($stmt, $types), $this->refValues($args)));
+                mysqli_stmt_bind_param($stmt, $types, ...$args);
                 mysqli_stmt_execute($stmt);
                 $res = mysqli_stmt_get_result($stmt);
                 if(!$res) {
